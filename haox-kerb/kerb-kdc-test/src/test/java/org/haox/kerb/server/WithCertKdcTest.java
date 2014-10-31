74d8caaff8a562190bcbce5e7ba1418dce94f960
Drankye <drankye@gmail.com>
2014-11-01 07:37:46 +0800

    Cleanup

--- haox-kerb/kerb-kdc-test/src/test/java/org/haox/kerb/server/WithCertKdcTest.java
+++ haox-kerb/kerb-kdc-test/src/test/java/org/haox/kerb/server/WithCertKdcTest.java
@@ -6,8 +6,20 @@ import org.haox.kerb.spec.type.ticket.TgtTicket;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Collection;
 
+/**
+ openssl genrsa -out cakey.pem 2048
+ openssl req -key cakey.pem -new -x509 -out cacert.pem -days 3650
+ vi extensions.kdc
+ openssl genrsa -out kdckey.pem 2048
+ openssl req -new -out kdc.req -key kdckey.pem
+ env REALM=SH.INTEL.COM openssl x509 -req -in kdc.req -CAkey cakey.pem \
+ -CA cacert.pem -out kdc.pem -days 365 -extfile extensions.kdc -extensions kdc_cert -CAcreateserial
+ */
 public class WithCertKdcTest extends KdcTestBase {
 
     private Certificate cert;
@@ -35,5 +47,11 @@ public class WithCertKdcTest extends KdcTestBase {
 
         ServiceTicket tkt = krbClnt.requestServiceTicket(tgt, serverPrincipal);
         Assert.assertNull(tkt);
+    }
+
+    private void loadCert() {
+        //CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+        //Collection<? extends Certificate> certs =
+        //        (Collection<? extends Certificate>) certFactory.generateCertificates(new ByteArrayInputStream(FileUtils.readFileToByteArray(serverCertFile)));
     }
 }
