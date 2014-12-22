c1a39407e16a3b23e05139714a940377fea61849
Drankye <drankye@gmail.com>
2014-12-22 21:07:02 +0800

    Fixed some building issue

--- haox-kerb/kerb-core-test/src/test/java/org/haox/kerb/codec/kerberos/KerberosApRequest.java
+++ haox-kerb/kerb-core-test/src/test/java/org/haox/kerb/codec/kerberos/KerberosApRequest.java
@@ -1,16 +1,11 @@
 package org.haox.kerb.codec.kerberos;
 
 import org.haox.kerb.codec.KrbCodec;
-import org.haox.kerb.codec.Util;
 import org.haox.kerb.spec.KrbException;
 import org.haox.kerb.spec.type.ap.ApOptions;
 import org.haox.kerb.spec.type.ap.ApReq;
 import org.haox.kerb.spec.type.common.EncryptionKey;
 
-import javax.security.auth.kerberos.KerberosKey;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class KerberosApRequest {
