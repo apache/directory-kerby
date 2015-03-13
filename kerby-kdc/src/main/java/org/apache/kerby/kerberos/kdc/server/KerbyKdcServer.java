cb5c66e920149439c9d06a2bc13af7915a84a050
hazel <linchen7@foxmail.com>
2015-03-13 14:13:12 +0800

    Add support for server load configure from config file And enhance kdc server to respect some more items in config file.

--- kerby-kdc/src/main/java/org/apache/kerby/kerberos/kdc/server/KerbyKdcServer.java
+++ kerby-kdc/src/main/java/org/apache/kerby/kerberos/kdc/server/KerbyKdcServer.java
@@ -19,10 +19,14 @@
  */
 package org.apache.kerby.kerberos.kdc.server;
 
+import org.apache.kerby.config.Conf;
 import org.apache.kerby.config.Config;
 import org.apache.kerby.kerberos.kdc.identitybackend.LdapIdentityBackend;
 import org.apache.kerby.kerberos.kerb.identity.IdentityService;
 import org.apache.kerby.kerberos.kerb.server.KdcServer;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * The mentioned Kerby KDC server implementation
@@ -38,7 +42,26 @@ public class KerbyKdcServer extends KdcServer {
         initIdentityService();
     }
 
-    private static KdcServer server;
+    public void init(String confDir, String workDir) throws IOException {
+        init();
+        initConfig(confDir);
+    }
+
+    /**
+     * init config from configuration file
+     */
+    private void initConfig(String confDirString) throws IOException {
+        Conf conf = kdcConfig.getConf();
+
+        File confDir = new File(confDirString);
+        File[] files = confDir.listFiles();
+        for (File file : files) {
+            conf.addIniConfig(file);
+        }
+
+    }
+
+    private static KerbyKdcServer server;
     private static final String USAGE = "Usage: " + KerbyKdcServer.class.getSimpleName() + " -start conf-dir working-dir|-stop";
 
     public static void main(String[] args) {
@@ -53,18 +76,17 @@ public class KerbyKdcServer extends KdcServer {
                 return;
             }
             String confDir = args[1];
-            String workingDir = args[2];
+            String workDir = args[2];
 
-            //FIXME host and config should be loaded from configuration.
-            String serverHost = "localhost";
-            short serverPort = 8015;
-
-            server = new KdcServer();
-            server.setKdcHost(serverHost);
-            server.setKdcTcpPort(serverPort);
-            server.init();
+            server = new KerbyKdcServer();
+            try {
+                server.init(confDir, workDir);
+            } catch (IOException e) {
+                System.err.println("Something wrong with configuration files or work files");
+                return;
+            }
             server.start();
-            System.out.println("KDC Server(" + KerbyKdcServer.class.getSimpleName() + ") started.");
+            System.out.println(KerbyKdcServer.class.getSimpleName() + " started.");
         } else if (args[0].equals("-stop")) {
             //server.stop();//FIXME can't get the server instance here
             System.out.println("KDC Server stoped.");
