b8ffa85a5a0af983c91f52be3cb307ab2edce2e7
hazel <linchen7@foxmail.com>
2015-03-19 13:38:53 +0800

    Add java debug option in kinit and kdc-server start up scripts.

--- kerby-dist/tool-dist/bin/kinit.sh
+++ kerby-dist/tool-dist/bin/kinit.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-confdir=/etc/kerby/krb/conf
-java -cp ../lib/kerb-client-1.0-SNAPSHOT-jar-with-dependencies.jar:../lib/kinit-1.0-SNAPSHOT.jar org.apache.kerby.kerberos.tool.kinit.Kinit $@
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=1045,server=y,suspend=n -cp ../lib/kerb-client-1.0-SNAPSHOT-jar-with-dependencies.jar:../lib/kinit-1.0-SNAPSHOT.jar org.apache.kerby.kerberos.tool.kinit.Kinit $@
