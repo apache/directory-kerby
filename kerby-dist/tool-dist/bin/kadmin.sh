2e1de10bca564fa618b171fed21cca4117a3ac68
Drankye <drankye@gmail.com>
2015-03-21 08:05:50 +0800

    Fixed script wrapers

--- kerby-dist/tool-dist/bin/kadmin.sh
+++ kerby-dist/tool-dist/bin/kadmin.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=1046,server=y,suspend=n -cp ../lib/kadmin-1.0-SNAPSHOT-jar-with-dependencies.jar org.apache.kerby.kerberos.tool.kadmin.Kadmin $@
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=1046,server=y,suspend=n \
+-cp -cp ../lib/kerb-client-1.0-SNAPSHOT-jar-with-dependencies.jar:../lib/client-tool-1.0-SNAPSHOT.jar \
+../lib/server-tool-1.0-SNAPSHOT.jar org.apache.kerby.kerberos.tool.kadmin.Kadmin $@
