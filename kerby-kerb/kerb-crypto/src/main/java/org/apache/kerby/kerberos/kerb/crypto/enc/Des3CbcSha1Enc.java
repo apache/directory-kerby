89f8593576b461eeccf9a423eb44d9a386670eb0
Drankye <drankye@gmail.com>
2015-04-24 00:01:13 +0800

    DIRKRB-217 Implement prf function in KeKiHmacSha1Enc. Thanks Yaning for the contribution

--- kerby-kerb/kerb-crypto/src/main/java/org/apache/kerby/kerberos/kerb/crypto/enc/Des3CbcSha1Enc.java
+++ kerby-kerb/kerb-crypto/src/main/java/org/apache/kerby/kerberos/kerb/crypto/enc/Des3CbcSha1Enc.java
@@ -6,29 +6,33 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
  *  under the License. 
- *  
+ *
  */
 package org.apache.kerby.kerberos.kerb.crypto.enc;
 
+import org.apache.kerby.kerberos.kerb.KrbException;
 import org.apache.kerby.kerberos.kerb.crypto.cksum.provider.Sha1Provider;
+import org.apache.kerby.kerberos.kerb.crypto.enc.provider.Aes128Provider;
 import org.apache.kerby.kerberos.kerb.crypto.enc.provider.Des3Provider;
+import org.apache.kerby.kerberos.kerb.crypto.key.AesKeyMaker;
 import org.apache.kerby.kerberos.kerb.crypto.key.Des3KeyMaker;
+import org.apache.kerby.kerberos.kerb.crypto.key.DkKeyMaker;
 import org.apache.kerby.kerberos.kerb.spec.base.CheckSumType;
 import org.apache.kerby.kerberos.kerb.spec.base.EncryptionType;
 
 public class Des3CbcSha1Enc extends KeKiHmacSha1Enc {
 
     public Des3CbcSha1Enc() {
-        super(new Des3Provider(), new Sha1Provider());
+        super(new Des3Provider(), new Sha1Provider(), new Des3KeyMaker(new Des3Provider()));
         keyMaker(new Des3KeyMaker(this.encProvider()));
     }
 
@@ -39,4 +43,5 @@ public class Des3CbcSha1Enc extends KeKiHmacSha1Enc {
     public CheckSumType checksumType() {
         return CheckSumType.HMAC_SHA1_DES3;
     }
+
 }
--- kerby-kerb/kerb-crypto/src/main/java/org/apache/kerby/kerberos/kerb/crypto/enc/KeKiHmacSha1Enc.java
+++ kerby-kerb/kerb-crypto/src/main/java/org/apache/kerby/kerberos/kerb/crypto/enc/KeKiHmacSha1Enc.java
@@ -6,28 +6,34 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
  *  under the License. 
- *  
+ *
  */
 package org.apache.kerby.kerberos.kerb.crypto.enc;
 
+import org.apache.kerby.kerberos.kerb.crypto.enc.provider.Aes128Provider;
+import org.apache.kerby.kerberos.kerb.crypto.key.AesKeyMaker;
+import org.apache.kerby.kerberos.kerb.crypto.key.DkKeyMaker;
 import org.apache.kerby.kerberos.kerb.crypto.util.Hmac;
 import org.apache.kerby.kerberos.kerb.crypto.cksum.HashProvider;
 import org.apache.kerby.kerberos.kerb.KrbException;
 
 public abstract class KeKiHmacSha1Enc extends KeKiEnc {
 
+    private DkKeyMaker km;
+
     public KeKiHmacSha1Enc(EncryptProvider encProvider,
-                           HashProvider hashProvider) {
+                           HashProvider hashProvider, DkKeyMaker km) {
         super(encProvider, hashProvider);
+        this.km = km;
     }
 
     @Override
@@ -36,9 +42,18 @@ public abstract class KeKiHmacSha1Enc extends KeKiEnc {
     }
 
     @Override
-    public byte[] prf(byte[] key, byte[] seed) {
-        // TODO: krb5int_dk_prf
-        return null;
+    public byte[] prf(byte[] key, byte[] seed) throws KrbException {
+        byte[] prfConst = "prf".getBytes();
+        int cksumSize = (hashProvider().hashSize() / encProvider().blockSize()) * encProvider().blockSize();
+        byte[] cksum = new byte[cksumSize];
+        byte[] kp;
+        byte[] output = new byte[prfSize()];
+        hashProvider().hash(seed);
+        System.arraycopy(hashProvider().output(), 0, cksum, 0, cksumSize);
+        kp = km.dk(key, prfConst);
+        encProvider().encrypt(kp, cksum);
+        System.arraycopy(cksum, 0, output, 0, this.prfSize());
+        return output;
     }
 
     @Override
