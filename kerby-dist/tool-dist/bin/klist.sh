8dde3f3fcfa7d6640415577feca0669b05bf7d49
plusplusjiajia <jiajia.li@intel.com>
2015-06-23 13:53:17 +0800

    Use the client and server api all jars in the tool.

--- kerby-dist/tool-dist/bin/klist.sh
+++ kerby-dist/tool-dist/bin/klist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 java -Xdebug -Xrunjdwp:transport=dt_socket,address=1045,server=y,suspend=n \
--cp ../lib/kerb-client-1.0-SNAPSHOT-jar-with-dependencies.jar:\
+-cp ../lib/kerb-client-api-all-1.0-SNAPSHOT.jar:\
+../lib/kerby-asn1-1.0-SNAPSHOT.jar:\
 ../lib/client-tool-1.0-SNAPSHOT.jar org.apache.kerby.kerberos.tool.klist.KlistTool $@
