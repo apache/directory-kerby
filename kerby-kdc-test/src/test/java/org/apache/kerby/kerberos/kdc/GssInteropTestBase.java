6eccd72934b574884018a19f75ccdd4460844eb8
drankye <kai.zheng@intel.com>
2015-07-01 00:31:25 +0800

    Clean up and fixed NPE

--- kerby-kdc-test/src/test/java/org/apache/kerby/kerberos/kdc/GssInteropTestBase.java
+++ kerby-kdc-test/src/test/java/org/apache/kerby/kerberos/kdc/GssInteropTestBase.java
@@ -54,19 +54,6 @@ public abstract class GssInteropTestBase extends KdcTestBase {
         return getClientPassword(); // Reuse the same password
     }
 
-    @Before
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
-        File file1 = new File(getClass().getResource("/kerberos.jaas").getPath());
-        String content1 = getFileContent(file1.getPath());
-        String path1 = writeToTestDir(content1, file1.getName());
-
-        // System.setProperty("sun.security.krb5.debug", "true");
-        System.setProperty("java.security.auth.login.config", path1);
-    }
-
     @Test
     public void testKdc() throws Exception {
         Subject clientSubject = JaasKrbUtil.loginUsingPassword(
