94c557794807bda3daec341edac1dbf70a2f3c80
Emmanuel Lécharny <elecharny@symas.com>
2015-08-22 12:01:02 +0200

    removed unusued declaratiuon

--- kerby-asn1/src/test/java/org/apache/kerby/asn1/TestTaggingEncoding.java
+++ kerby-asn1/src/test/java/org/apache/kerby/asn1/TestTaggingEncoding.java
@@ -168,8 +168,6 @@ public class TestTaggingEncoding {
         Type1 aType1 = new Type1(TEST_STRING);
         Type2 aType2 = new Type2(aType1);
         Type3 aType3 = new Type3(aType2);
-        Type4 aType4 = new Type4(aType3);
-        Type5 aType5 = new Type5(aType2);
 
         assertThat(aType1.encode()).isEqualTo(TYPE1_EXPECTED_BYTES);
         assertThat(TYPE2_EXPECTED_BYTES)
