657a5b56ff14793e3524925da20359a0ead44c2c
Colm O hEigeartaigh <coheigea@apache.org>
2015-09-30 12:23:33 +0200

    Minor fix

--- kerby-kdc-test/src/test/java/org/apache/kerby/kerberos/kdc/WithTokenKdcTestBase.java
+++ kerby-kdc-test/src/test/java/org/apache/kerby/kerberos/kdc/WithTokenKdcTestBase.java
@@ -78,7 +78,7 @@ public class WithTokenKdcTestBase extends KdcTestBase {
         authToken.setAudiences(aud);
 
         // Set expiration in 60 minutes
-        final Date now =  new Date(new Date().getTime() / 1000 * 1000);
+        final Date now = new Date();
         Date exp = new Date(now.getTime() + 1000 * 60 * 60);
         authToken.setExpirationTime(exp);
 
