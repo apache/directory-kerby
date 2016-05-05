e53e5ff4cb2c93bb230e03cfae4459f9029ac9d0
AliceYan <yan.a.yan@intel.com>
2016-05-05 14:36:38 +0800

    update AdminServerInit to get the right kdcConfig and backendConfig

--- kerby-kerb/kerb-admin-server/src/main/java/org/apache/kerby/kerberos/kerb/admin/server/AdminServerInit.java
+++ kerby-kerb/kerb-admin-server/src/main/java/org/apache/kerby/kerberos/kerb/admin/server/AdminServerInit.java
@@ -24,6 +24,7 @@ import org.apache.kerby.kerberos.kerb.KrbException;
 import org.apache.kerby.kerberos.kerb.admin.server.kadmin.AdminServer;
 import org.apache.kerby.kerberos.kerb.admin.server.kadmin.AdminServerConfig;
 import org.apache.kerby.kerberos.kerb.identity.backend.BackendConfig;
+import org.apache.kerby.kerberos.kerb.server.KdcConfig;
 import org.apache.kerby.util.OSUtil;
 
 import java.io.File;
@@ -36,7 +37,7 @@ public class AdminServerInit {
         + "\t\t"
         + (OSUtil.isWindows()
         ? "bin\\kadmin.cmd" : "sh bin/kadmin.sh")
-        + " conf\\adminServer.conf\n";
+        + " conf\n";
 
     public static void main(String[] args) throws Exception {
 
@@ -46,11 +47,21 @@ public class AdminServerInit {
         }
 
         String confDirPath = args[0];
-        File serverConfFile = new File(confDirPath);
+        File adminServerConfFile = new File(confDirPath, "adminServer.conf");
+        File kdcConfFile = new File(confDirPath, "kdc.conf");
+        File backendConfFile = new File(confDirPath, "backend.conf");
 
-        if (!serverConfFile.exists()) {
-            System.err.println("Invalid or not exist conf");
+        if (!adminServerConfFile.exists()) {
+            System.err.println("Invalid or not exist adminServer conf");
             System.exit(2);
+        }
+        if (!kdcConfFile.exists()) {
+            System.err.println("Invalid or not exist kdc conf");
+            System.exit(3);
+        }
+        if (!backendConfFile.exists()) {
+            System.err.println("Invalid or not exist backend conf");
+            System.exit(4);
         }
 
         /**
@@ -58,13 +69,16 @@ public class AdminServerInit {
          * Because url is not hierarchical
          */
         //URL serverConfFileUrl = AdminServerInit.class.getResource("/adminServer.conf");
-        //File serverConfFile = new File(serverConfFileUrl.toURI());
+        //File adminServerConfFile = new File(serverConfFileUrl.toURI());
         AdminServerConfig adminServerConfig = new AdminServerConfig();
-        adminServerConfig.addKrb5Config(serverConfFile);
+        adminServerConfig.addKrb5Config(adminServerConfFile);
 
         BackendConfig backendConfig = new BackendConfig();
+        backendConfig.addIniConfig(backendConfFile);
+        KdcConfig kdcConfig = new KdcConfig();
+        kdcConfig.addKrb5Config(kdcConfFile);
 
-        AdminServer adminServer = new AdminServer(adminServerConfig, backendConfig);
+        AdminServer adminServer = new AdminServer(adminServerConfig, backendConfig, kdcConfig);
         adminServer.setAdminHost(adminServerConfig.getAdminHost());
         adminServer.setAllowTcp(true);
         adminServer.setAllowUdp(false);
